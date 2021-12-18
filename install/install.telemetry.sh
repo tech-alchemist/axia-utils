020518d6c88ea0745af844c7b9b7a9bed8705d82
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-12-18 15:06:16 +0530

    Additions

--- install/install.telemetry.sh
+++ install/install.telemetry.sh
@@ -9,27 +9,25 @@ CLEANUP="$1" ; [[ -z ${CLEANUP} ]] && CLEANUP="false"
 PM2="/usr/local/bin/pm2"
 
 ## Install required packages ##
-which npm || {
-	echo "[+] Well, It's a fresh Node, Setting up from scratch."
-	sudo apt install npm jq net-tools git curl libssl-dev libffi-dev clang llvm libudev-dev expect wget
-	echo "[+] Setting Up Rust"
-	curl -sSLk https://sh.rustup.rs -o /tmp/rustup.sh
+echo "[+] Well, It's a fresh Node, Setting up from scratch."
+sudo apt install npm jq net-tools git curl libssl-dev libffi-dev clang llvm libudev-dev expect wget
+echo "[+] Setting Up Rust"
+curl -sSLk https://sh.rustup.rs -o /tmp/rustup.sh
 echo '#!/usr/bin/expect -f
 set timeout -1
 spawn /tmp/rustup.sh
 send -- "1\r"
 expect eof' > /tmp/rustup.exp
-	chmod +x /tmp/rustup.*
-	/tmp/rustup.exp ; 
-	source $HOME/.cargo/env
-	rm -f /tmp/rustup.*
-	echo "[+] Setting up NodeJS"
-	sudo npm i -g n
-	sudo n 14.18
-	echo "[+] Installed NodeJS $(node -v) & NPM $(npm -v)"
-	sudo npm i -g pm2 yarn || exit 1
-	echo "[+] Installed pm2 & yarn (latest)"
-}
+chmod +x /tmp/rustup.*
+/tmp/rustup.exp ; 
+source $HOME/.cargo/env
+rm -f /tmp/rustup.*
+echo "[+] Setting up NodeJS"
+sudo npm i -g n
+sudo n 14.18
+echo "[+] Installed NodeJS $(node -v) & NPM $(npm -v)"
+sudo npm i -g pm2 yarn || exit 1
+echo "[+] Installed pm2 & yarn (latest)"
 
 ## CleanUp ##
 [[ ${CLEANUP} == "true" ]] && {
