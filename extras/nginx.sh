fec3fe1f5b56649f8d874c574f5f58241d4b6c48
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-12-18 20:17:01 +0530

    When angry, count four; when very angry, swear.

--- extras/nginx.sh
+++ extras/nginx.sh
@@ -7,7 +7,7 @@ PRIVATE_IP="$2"
 PRIVATE_PORT="$3"
 UTILS_DIR="/opt/opsdude/axia-utils/extras"
 
-[[ ! -z ${DOMAIN_NAME} ]] || [[ ! -z "${PRIVATE_IP}" ]] || [[ ! -z ${PRIVATE_PORT} ]] || { echo "[-] Usage : $0 <WSS/HTTPS> <DOMAINNAME> <PROXYHOST> <PROXYPORT>" ; exit 1 ; } 
+[[ ! -z ${DOMAIN_NAME} ]] || [[ ! -z "${PRIVATE_IP}" ]] || [[ ! -z ${PRIVATE_PORT} ]] || { echo "[-] Usage : $0 <DOMAINNAME> <PROXYHOST> <PROXYPORT>" ; exit 1 ; } 
 
 sed -e "s/DOMAIN_NAME/${DOMAIN_NAME}/g" -e "s|PRIVATE_IP|${PRIVATE_IP}|g" -e "s|PRIVATE_PORT|${PRIVATE_PORT}|g" ${UTILS_DIR}/nginx.conf.example | sudo tee /etc/nginx/sites-enabled/${DOMAIN_NAME}.conf
 sudo service nginx restart
