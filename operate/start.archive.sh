a81a437b67cea52a61f219cce03b67ecaf58a866
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-12-24 01:06:32 +0530

    Checksum kept on remote node

--- operate/start.archive.sh
+++ operate/start.archive.sh
@@ -27,8 +27,7 @@ file_sanity(){
   FILE_URL="$3"
   CHECKSUM_URL="https://releases.axiacoin.network/stable/checksums.txt"
   CHECKSUM_FILE="/tmp/.checksum.txt"
-  rm -f ${CHECKSUM_FILE} ; wget -c "${CHECKSUM_URL}" -q -O ${CHECKSUM_FILE} || : echo "[-] Unable to downlaod checksums from ${CHECKSUM_URL}"  ; exit 1 ; }
-  MENTIONED_HASH="$(grep -i " ${FILE_NAME}" ${CHECKSUM_FILE}|awk '{print $1}'| head -1)"
+  rm -f ${CHECKSUM_FILE} ; wget -c "${CHECKSUM_URL}" -q -O ${CHECKSUM_FILE} || { echo "[-] Unable to downlaod checksums from ${CHECKSUM_URL}"  ; exit 1 ; }  MENTIONED_HASH="$(grep -i " ${FILE_NAME}" ${CHECKSUM_FILE}|awk '{print $1}'| head -1)"
   EXISTING_HASH="$(md5sum ${FILE_PATH} | awk '{print $1}')"
   [[ "${MENTIONED_HASH}" != "${EXISTING_HASH}" ]] && { 
     rm -f ${FILE_PATH}
